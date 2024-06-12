b5d1f38a70e729c3a82d424b5727c7062da784a4
Spyyydi <>
2024-06-12 19:33:32 +0300

    Added new class for not allocated actors report

---
+++ Ass to Srt roles allocator/Ass to Srt roles allocator/ShowLongMessage.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Ass_to_Srt_roles_allocator
+{
+    partial class ShowLongMessage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShowLongMessage));
+            this.richTxtMessage = new System.Windows.Forms.RichTextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTxtMessage
+            // 
+            this.richTxtMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTxtMessage.DetectUrls = false;
+            this.richTxtMessage.Location = new System.Drawing.Point(0, 0);
+            this.richTxtMessage.Name = "richTxtMessage";
+            this.richTxtMessage.ReadOnly = true;
+            this.richTxtMessage.Size = new System.Drawing.Size(400, 300);
+            this.richTxtMessage.TabIndex = 0;
+            this.richTxtMessage.Text = "";
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(150, 302);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 25);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Ok";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // ShowLongMessage
+            // 
+            this.AcceptButton = this.btnClose;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(400, 329);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.richTxtMessage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ShowLongMessage";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Not allocated actors";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTxtMessage;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
